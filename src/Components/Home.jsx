45223cb8eefdba6b84edba26da533845d485e68f
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-08-19 22:28:38 +0700

    Removing cached files

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
 const Home = () => {
-    
-    
     return (
         <main className="main">
             <a href="/stages" className='main__cta'>Let's Start Our Journey</a>
