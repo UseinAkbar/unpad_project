57dbbd527cbe8d80a40b51f860cd59682269a881
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-10-01 23:57:46 +0700

    responsive home and make info page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './assets/tailwind.css';
 import './assets/style.css';
+import './assets/tailwind.css';
 
 ReactDOM.render (
     <App />,
